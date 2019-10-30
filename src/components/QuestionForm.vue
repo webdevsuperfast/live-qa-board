49464125ef0ecf77d703e13996432b48c883b285
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-30 08:35:04 +0800

    Code cleanup

--- src/components/QuestionForm.vue
+++ src/components/QuestionForm.vue
@@ -1,53 +1,28 @@
 <template>
   <div class="question-form">
-    <notification-container
-      :status="status"
-    />
+    <notification-container :status="status" />
     <form method="post" @submit.prevent="handleSubmit">
       <input type="hidden" name="form-name" value="ask-question" />
       <ul>
-      <li>
-        <label>
-          Your Name:
-          <input
-            type="text"
-            name="name"
-            @input="ev => form.name = ev.target.value"
-            >
-        </label>
-      </li>
-      <li>
-        <p>Ask:</p>
-        <label
-          :class="{
-            'pick-panelist': true,
-            'checked':form.askPerson === panelist,
-            'disabled': ifEvan(panelist)
-          }"
-          v-for="panelist in panelists"
-        >
-          <input
-            type="radio"
-            name="panelist"
-            @input="ev => form.askPerson = ev.target.value"
-            :value="panelist"
-            :disabled="ifEvan(panelist)"
-            :checked="form.askPerson === panelist"
-          >
-          <span>{{ panelist }}</span>
-        </label>
-      </li>
-      <li>
-        <label>
-          Your Question:
-          <textarea
-             ref="input"
-             name="question"
-             @input="ev => form.question = ev.target.value"
-             placeholder="Question Goes Here"
-          />
-        </label>
-      </li>
+        <li>
+          <label>
+            Your Name:
+            <input type="text" name="name" @input="ev => form.name = ev.target.value" />
+          </label>
+        </li>
+        <li>
+          <p>Ask:</p>
+          <label :class="{'pick-panelist': true, 'checked': form.askPerson === panelist, 'disabled': ifEvan(panelist)}" v-for="(panelist, index) in panelists" :key="index">
+            <input type="radio" name="panelist" @input="ev => form.askPerson = ev.target.value" :value="panelist" :disabled="ifEvan(panelist)" :checked="form.askPerson === panelist" />
+            <span>{{ panelist }}</span>
+          </label>
+        </li>
+        <li>
+          <label>
+            Your Question:
+            <textarea ref="input" name="question" @input="ev => form.question = ev.target.value" placeholder="Question Goes Here" />
+          </label>
+        </li>
       </ul>
       <button type="submit" class="submit-button">Ask a question</button>
     </form>
