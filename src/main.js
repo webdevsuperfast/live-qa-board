8f445d4d2d0fbf64dc9868603061e2e205119e31
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-30 08:34:41 +0800

    Added missing router configuration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from "vue";
+
+import QAForm from './components/QuestionForm'
+import SubmissionSuccess from './components/SubmissionSuccess'
+import SubmissionFail from './components/SubmissionFail'
+
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'QA Form',
+      component: QAForm
+    },
+    {
+      path: '/thanks',
+      name: 'success',
+      component: SubmissionSuccess
+    },
+    {
+      path: '/404',
+      name: 'fail',
+      component: SubmissionFail
+    }
+  ]
+})
+
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
