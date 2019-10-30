17df29bc5f4ab404423d37849a7b18213c1c4d82
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-30 09:04:58 +0800

    Use already present router configuration

--- src/main.js
+++ src/main.js
@@ -1,34 +1,7 @@
 import Vue from "vue";
-
-import QAForm from './components/QuestionForm'
-import SubmissionSuccess from './components/SubmissionSuccess'
-import SubmissionFail from './components/SubmissionFail'
-
-import Router from 'vue-router'
-
-Vue.use(Router)
-
-const router = new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'QA Form',
-      component: QAForm
-    },
-    {
-      path: '/thanks',
-      name: 'success',
-      component: SubmissionSuccess
-    },
-    {
-      path: '/404',
-      name: 'fail',
-      component: SubmissionFail
-    }
-  ]
-})
-
 import App from "./App.vue";
+
+import router from './router/index'
 
 Vue.config.productionTip = false;
 
