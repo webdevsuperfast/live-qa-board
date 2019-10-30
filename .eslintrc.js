d31d4a86c640428ee6f8187b353f0928264bc40b
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-30 08:35:27 +0800

    Hide all warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,17 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/prettier'
+    'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        'args': 'none'
+      }
+    ]
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
